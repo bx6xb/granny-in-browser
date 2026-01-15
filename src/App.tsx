96b405935c5008a874f8277bd86197fda0fedfe4
Yan Turnt <bx6xbchosen1@gmail.com>
2026-01-15 14:37:34 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return (
+    <div className="app-root">
+      <Canvas style={{ background: "green", width: "100vw", height: "100vh" }}>
+        <axesHelper args={[5]} />
+        <mesh></mesh>
+
+        <directionalLight position={[2, 2, 2]} intensity={10} />
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
