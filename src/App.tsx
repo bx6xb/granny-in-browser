1622710ed2f377e3f7d666564d1ba983de011062
Yan Turnt <bx6xbchosen1@gmail.com>
2026-01-19 19:54:05 +0500

    feat: crosshair for player

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', background: '#000000' }}>
       <GameUI />
+      <div className="crosshair" />
       <Canvas 
         shadows 
         camera={{ position: [0, 0, 0], fov: 75 }}
